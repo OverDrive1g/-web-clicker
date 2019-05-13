d3893c8b489c81631f8fa2b15914962f66117309
Vladislav Bedilo <v.overdrive@gmail.com>
2019-05-14 01:52:41 +0300

    add hp bar

--- src/components/Clicker.vue
+++ src/components/Clicker.vue
@@ -4,8 +4,14 @@
     >
         <v-img
                 :src="require('../assets/pig-coin.svg')"
-                v-on:click="add"
+                v-on:click="onClick"
         />
+        <v-progress-linear
+                color="error"
+                height="20"
+                :value="hp"
+                class="text-md-center"
+        >{{hp}}</v-progress-linear>
     </div>
 </template>
 
@@ -14,10 +20,22 @@
 
     export default {
         name: "Clicker",
+        data(){
+            return {
+                hp:100
+            }
+        },
         methods: {
             ...mapMutations({
                 add:'increment'
-            })
+            }),
+            onClick:function(){
+                this.hp = this.hp - 20;
+                if(this.hp < 0){
+                    this.hp = 100
+                }
+                this.add()
+            }
         },
     }
 </script>
