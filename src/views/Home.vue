ab7d74e933d8da92e6072ba64309ae87666bf8a9
Vladislav Bedilo <v.overdrive@gmail.com>
2019-05-17 00:35:38 +0300

    :)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,19 +42,19 @@
         {property: 'og:type', content: 'website'},
         {property: 'og:url', content: 'https://cl1cker.online/'},
         {property: 'og:image', content: 'https://cl1cker.online/logo.png'},
-        {property: 'og:description', content: 'Awesome clicker with little pig'},
+        {property: 'og:description', content: 'Cl1cker.online is awesome clicker game with little pig. You do not agree? Then just click a couple of times.'},
 
         // Twitter card
         {name: 'twitter:card', content: 'summary'},
         {name: 'twitter:site', content: 'https://cl1cker.online/'},
         {name: 'twitter:title', content: 'Pig-Clicker'},
-        {name: 'twitter:description', content: 'Awesome clicker with little pig'},
+        {name: 'twitter:description', content: 'Cl1cker.online is awesome clicker game with little pig. You do not agree? Then just click a couple of times.'},
         {name: 'twitter:creator', content: '@OverDrive1g'},
         {name: 'twitter:image:src', content: 'https://cl1cker.online/logo.png'},
 
         // Google / Schema.org markup:
         {itemprop: 'name', content: 'Pig-Clicker'},
-        {itemprop: 'description', content: 'Awesome clicker with little pig'},
+        {itemprop: 'description', content: 'Cl1cker.online is awesome clicker game with little pig. You do not agree? Then just click a couple of times.'},
         {itemprop: 'image', content: 'https://cl1cker.online/logo.png'}
       ],
       link: [
