1ee6df6ed9ca48f48214f37d4f3beeec34152b80
Vladislav Bedilo <v.overdrive@gmail.com>
2019-05-19 01:01:23 +0300

    fix damage controller

--- src/core/DamageController.js
+++ src/core/DamageController.js
@@ -1,6 +1,8 @@
-export class DamageController{
+class DamageController{
+    constructor(){}
+
     random(n){
-        Math.round(Math.random()*n)
+        return Math.round(Math.random()*n)
     }
     rollDice(N,S){
         let value = 0
@@ -15,11 +17,12 @@ export class DamageController{
         let roll3 = this.rollDice(1, 50)
         let damage = roll1 + roll2 + roll3
         damage = damage - Math.min(roll1, roll2, roll3)
-        return damage
+        return (damage===0?0.1:damage)/100
     }
 
     getDamageByAttack(attack){
         let deltaAttack = attack/4
-        return deltaAttack+(attack*(this.getDamagePattern()/100))
+        return deltaAttack+(attack*this.getDamagePattern())
     }
 }
+export default DamageController
