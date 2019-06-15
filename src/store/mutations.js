1a3b624df6eec6fd6ba5f91a21ac96263796c7cf
Vladislav Bedilo <v.overdrive@gmail.com>
2019-06-16 02:16:21 +0300

    release new alg for load save

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,9 +1,23 @@
 export default {
 
     loadSave(state, payload){
-        state.count = payload.count
-        state.attack = payload.attack
-        state.upgradeList = payload.upgradeList
+        console.log(payload )
+        state.mob.lvl = payload.mob.lvl
+
+        state.player.attack = payload.player.attack
+        state.player.coins = payload.player.coins
+
+        state.statistic.clicks = payload.statistic.clicks
+
+        let upgrades = payload.upgrade
+
+        for (let i = 0; i < upgrades.length; i++) {
+            let found = state.player.upgradeList.find(itm=>itm.id === upgrades[i].id)
+            if(found){
+                found.count = upgrades[i].count
+            }
+        }
+
     },
     setSaveId(state, id){
         state.saveId = id
