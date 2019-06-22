f812905921fefc8c484c1d18f40ffd4bed608658
Vladislav Bedilo <v.overdrive@gmail.com>
2019-06-23 00:59:03 +0300

    fix coins on boss and add show kills on lvl

--- src/components/Statistic.vue
+++ src/components/Statistic.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="pa-3">
+        <p>Killed on lvl: {{killedOnLvl}}/10</p>
         <p>Coins: {{Math.round(coins * 100) / 100}}</p>
         <p>Attack: {{Math.round(attack * 100) / 100}}</p>
         <p>Clicks: {{Math.round(clicks * 100) / 100}}</p>
@@ -14,6 +15,7 @@
         computed: {
             ...mapState('statistic',["clicks","coins"]),
             ...mapState('player',["coins","attack"]),
+            ...mapState('mob',["killedOnLvl"]),
         }
     }
 </script>
