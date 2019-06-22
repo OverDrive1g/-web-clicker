1b693a780bf69ce4b131320d0d5858d5c5090e75
Vladislav Bedilo <v.overdrive@gmail.com>
2019-06-23 01:00:57 +0300

    replace icons

--- src/core/AchievementsController.js
+++ src/core/AchievementsController.js
@@ -19,71 +19,71 @@ class AchievementsController{
                 this.setReward({type:"coin", count:50})
                 break
             case 1000:
-                this.showSnackbar("I did not sleep for two days","info")
+                this.showSnackbar("I did not sleep for two days","alarm")
                 this.setReward({type:"coin", count:500})
                 break
             case 10000:
-                this.showSnackbar("mmmm","info")
+                this.showSnackbar("mmmm","pan_tool")
                 this.setReward({type:"coin", count:5000})
                 break
             case 100000:
-                this.showSnackbar("Overdose","info")
+                this.showSnackbar("Overdose","gavel")
                 this.setReward({type:"coin", count:50000})
                 break
             case 1000000:
-                this.showSnackbar("Oh yes, do not stop","info")
+                this.showSnackbar("Oh yes, do not stop","adjust")
                 this.setReward({type:"coin", count:500000})
                 break
             case 10000000:
-                this.showSnackbar("AAAAAAAAA","info")
+                this.showSnackbar("AAAAAAAAA","format_color_text")
                 this.setReward({type:"coin", count:5000000})
                 break
             case 100000000:
-                this.showSnackbar("You can not stop","info")
+                this.showSnackbar("You can not stop","tag_faces")
                 this.setReward({type:"coin", count:50000000})
                 break
             case 1000000000:
-                this.showSnackbar("CLICKS UNTIL THE END","info")
+                this.showSnackbar("CLICKS UNTIL THE END","send")
                 this.setReward({type:"coin", count:500000000})
                 break
             case 10000000000:
-                this.showSnackbar("How?","info")
+                this.showSnackbar("How?","whatshot")
                 this.setReward({type:"coin", count:5000000000})
                 break
             case 100000000000:
-                this.showSnackbar("He is controlled by all the clicks of the world","info")
+                this.showSnackbar("He is controlled by all the clicks of the world","public")
                 this.setReward({type:"coin", count:50000000000})
                 break
             case 1000000000000:
-                this.showSnackbar("WE NEED MORE CLICKS","info")
+                this.showSnackbar("WE NEED MORE CLICKS","exposure_plus_1")
                 this.setReward({type:"coin", count:500000000000})
                 break
             case 10000000000000:
-                this.showSnackbar("PANIC!!!!","info")
+                this.showSnackbar("PANIC!!!!","wifi_off")
                 this.setReward({type:"coin", count:5000000000000})
                 break
             case 100000000000000:
-                this.showSnackbar("Where I am?","info")
+                this.showSnackbar("Where I am?","nature_people")
                 this.setReward({type:"coin", count:50000000000000})
                 break
             case 1000000000000000:
-                this.showSnackbar("How did you get here?","info")
+                this.showSnackbar("How did you get here?","directions_run")
                 this.setReward({type:"coin", count:500000000000000})
                 break
             case 10000000000000000:
-                this.showSnackbar("Okay ending","info")
+                this.showSnackbar("Okay ending","work_off")
                 this.setReward({type:"coin", count:5000000000000000})
                 break
             case 100000000000000000:
-                this.showSnackbar("Well, no, I'm serious","info")
+                this.showSnackbar("Well, no, I'm serious","cloud_off")
                 this.setReward({type:"coin", count:50000000000000000})
                 break
             case 1000000000000000000:
-                this.showSnackbar("STOP","info")
+                this.showSnackbar("STOP","sentiment_satisfied")
                 this.setReward({type:"coin", count:500000000000000000})
                 break
             case 10000000000000000000:
-                this.showSnackbar("You exceeded the limit will now be fucked","info")
+                this.showSnackbar("You exceeded the limit will now be fucked","import_contacts")
                 this.setReward({type:"coin", count:5000000000000000000})
                 break
         }
